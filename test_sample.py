5bf546edb1432d35a5df2ef29601f26782ef1d80
Yogesh Jani <yogesh.jani16@gmail.com>
2025-10-01 12:44:05 +0530

    updated test case

--- test_sample.py
+++ test_sample.py
@@ -10,10 +10,10 @@ def test_case2():
     assert inc(110) == 111
 
 def test_case3():
-    assert inc(3) == 5
+    assert inc(4) == 5
 
 def test_case4():
     assert inc(8) == 9
 
 def test_case5():
-    assert inc(10) == 12
+    assert inc(11) == 12
